35d698a325d6cbb9fd3fdfa6b607e44f2ea09b4c
Uriel <ubonano@gmail.com>
2016-04-01 20:02:19 -0300

    Modelo
    Migración tabla usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,22 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 50);
+            $table->string('lastName', 50);
+            $table->string('email', 60)->unique();
+            $table->string('password', 30);
+            $table->string('pais', 50);
+            $table->string('provincia', 50);
+            $table->string('localidad', 50);
+            $table->string('codigoPostal' 20);
+            $table->string('domicilio', 50);
+            $table->string('genero', 10);
+            $table->string('telefono', 50);
+            $table->string('cuit_cuil' 50);
+            $table->smallInteger('activo');
+            $table->date('nacimiento');
+            $table->smallInteger('tipoUsuario');
+            //$table-> referencia a la tabla cv
             $table->rememberToken();
             $table->timestamps();
         });
